2d52b9dee634fb5fdb092576ea168f8dd4119530
usigna <sylwia.fait@gmail.com>
2024-08-04 14:40:01 +0200

    Add base files

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Web Accessibility
+function handleFirstTab(e) {
+  if (e.key === 'Tab') {
+    document.body.classList.add('user-is-tabbing');
+    window.removeEventListener('keydown', handleFirstTab);
+  }
+};
+
+window.addEventListener('keydown', handleFirstTab);
+
